8d1676f03f0f6330794533ed20bc19a71b356b48
LincKim <lincolnkimutai@gmail.com>
2023-02-16 15:49:20 +0300

    Started assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
   # write code here
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind(equilateral, isosceles, scalene)
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+
+  end
 end
